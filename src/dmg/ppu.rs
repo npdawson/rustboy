d9e72aa0b578350ce5231afa641d263097ff8925
Nathaniel P. Dawson <nathaniel.dawson@gmail.com>
2016-09-23 14:30:58 -0700

    Started w/ events, refactored a lot
    ready for rendering to window, need to read up more on gameboy ppu

--- src/dmg/ppu.rs
+++ src/dmg/ppu.rs
@@ -1,21 +1,22 @@
-const FB_SIZE: usize = 160*144;
-const VRAM_START: usize = 0x8000;
-const OAM_START: usize = 0xFE00;
-const WHITE: [u8; 4] = [255, 255, 255, 255];
-const LGRAY: [u8; 4] = [192, 192, 192, 255];
-const DGRAY: [u8; 4] = [ 96,  96,  96, 255];
-const BLACK: [u8; 4] = [  0,   0,   0, 255];
+// const FB_SIZE: usize = 160*144;
+// const WHITE: [u8; 4] = [255, 255, 255, 255];
+// const LGRAY: [u8; 4] = [192, 192, 192, 255];
+// const DGRAY: [u8; 4] = [ 96,  96,  96, 255];
+// const BLACK: [u8; 4] = [  0,   0,   0, 255];
 
 use byteorder::{ByteOrder, LittleEndian};
 
-pub struct Gpu {
-    pub vram: Vec<u8>,
-    pub oam: Vec<u8>,
+#[derive(Debug)]
+pub struct Ppu {
+    pub vram: Box<[u8]>,
+    pub oam: Box<[u8]>,
 
-    fb: Vec<u32>,
+    // fb: Box<[u32]>,
     mode: Mode,
     modeclock: usize,
-    pub line: u8, // 160 lines
+    pub line: u8, // LY: 160 lines
+    // LY Compare
+    pub lyc: u8,
     // LCD CTRL, make separate struct?
     switchbg: bool,
     bgmap: bool,
@@ -27,28 +28,29 @@ pub struct Gpu {
     mode1vblank_int: bool,
     mode0hblank_int: bool,
     // Scroll coords
-    scy: u8,
-    scx: u8,
+    pub scy: u8,
+    pub scx: u8,
     // Window coords
-    wy: u8,
-    wx: u8,
+    pub wy: u8,
+    pub wx: u8,
     // background palette
-    bgp: Vec<Vec<u8>>,
+    pub bgp: u8,
     // Object palettes
-    obp0: Vec<Vec<u8>>,
-    obp1: Vec<Vec<u8>>,
+    pub obp0: u8,
+    pub obp1: u8,
 }
 
-impl Gpu {
-    pub fn new() -> Gpu {
-        Gpu {
-            vram: vec![0; 0x2000],
-            oam: vec![0; 0xA0],
+impl Ppu {
+    pub fn new() -> Ppu {
+        Ppu {
+            vram: vec![0; 0x2000].into_boxed_slice(),
+            oam: vec![0; 0xA0].into_boxed_slice(),
 
-            fb: vec![0xFF; FB_SIZE], // initialize to white screen
+            // fb: vec![0xFF; FB_SIZE], // initialize to white screen
             mode: Mode::Oam,
             modeclock: 0,
             line: 0,
+            lyc: 0,
 
             coincidence_int: false,
             mode2oam_int: false,
@@ -67,9 +69,9 @@ impl Gpu {
             wx: 0,
 
             // perhaps store these as single bytes, then convert to RGBA when needed
-            bgp:  vec![WHITE.to_vec(), WHITE.to_vec(), WHITE.to_vec(), BLACK.to_vec()],
-            obp0: vec![WHITE.to_vec(), WHITE.to_vec(), WHITE.to_vec(), WHITE.to_vec()],
-            obp1: vec![WHITE.to_vec(), WHITE.to_vec(), WHITE.to_vec(), WHITE.to_vec()],
+            bgp:  0xFC,
+            obp0: 0x00,
+            obp1: 0x00,
         }
     }
 
@@ -114,79 +116,74 @@ impl Gpu {
         }
     }
 
-    pub fn read_byte(&self, addr: usize) -> u8 {
-        match addr {
-            0xFF40 => self.lcd_ctrl(), // LCD Control Reg
-            0xFF41 => self.read_lcd_stat(),
-            0xFF42 => self.scy,
-            0xFF43 => self.scx,
-            0xFF44 => self.line, // current scanline
-            0xFF47 => panic!("Background Palette is Write-Only!"), // TODO
-            0xFF48 => panic!("Object Palette 0 is Write-Only!"), // TODO
-            0xFF49 => panic!("Object Palette 1 is Write-Only!"), // TODO
-            0xFF4A => self.wy,
-            0xFF4B => self.wx,
-            _ => panic!("Reading from GPU IOReg {:#X} not yet implemented!", addr)
-        }
-    }
+    // pub fn read_byte(&self, addr: u16) -> u8 {
+    //     match addr {
+    //         0xFF40 => self.read_lcd_ctrl(), // LCD Control Reg
+    //         0xFF41 => self.read_lcd_stat(),
+    //         0xFF42 => self.scy,
+    //         0xFF43 => self.scx,
+    //         0xFF44 => self.line, // current scanline
+    //         0xFF45 => self.lyc,
+    //         0xFF47 => panic!("Background Palette is Write-Only!"), // TODO
+    //         0xFF48 => panic!("Object Palette 0 is Write-Only!"), // TODO
+    //         0xFF49 => panic!("Object Palette 1 is Write-Only!"), // TODO
+    //         0xFF4A => self.wy,
+    //         0xFF4B => self.wx,
+    //         _ => panic!("Reading from PPU IOReg {:#X} not yet implemented!", addr)
+    //     }
+    // }
 
-    pub fn write_byte(&mut self, value: u8, addr: usize) {
-        match addr {
-            0xFF40 => self.set_lcd_ctrl(value),
-            0xFF41 => self.write_lcd_stat(value),
-            0xFF42 => self.scy = value,
-            0xFF43 => self.scx = value,
-            0xFF44 => println!("Can't write to 0xFF44."),
-            0xFF47 => set_palette(&mut self.bgp, value), // BG Palette
-            0xFF48 => set_palette(&mut self.obp0, value), // Object Palette 0
-            0xFF49 => set_palette(&mut self.obp1, value), // Object Palette 0
-            0xFF4A => self.wy = value,
-            0xFF4B => self.wx = value,
-            0xFF7F => println!("oopsy! 0xFF7F"),
-            _ => panic!("Writing to GPU IOReg {:#X} not yet implemented!", addr)
-        }
-    }
+    // pub fn write_byte(&mut self, addr: u16, value: u8) {
+    //     match addr {
+    //         0xFF40 => self.write_lcd_ctrl(value),
+    //         0xFF41 => self.write_lcd_stat(value),
+    //         0xFF42 => self.scy = value,
+    //         0xFF43 => self.scx = value,
+    //         0xFF44 => println!("Can't write to 0xFF44."),
+    //         0xFF45 => self.lyc = value,
+    //         0xFF47 => self.bgp = value, // BG Palette
+    //         0xFF48 => self.obp0 = value, // Object Palette 0
+    //         0xFF49 => self.obp1 = value, // Object Palette 0
+    //         0xFF4A => self.wy = value,
+    //         0xFF4B => self.wx = value,
+    //         0xFF7F => println!("oopsy! 0xFF7F"),
+    //         _ => panic!("Writing to PPU IOReg {:#X} not yet implemented!", addr)
+    //     }
+    // }
 
     pub fn read_vram(&self, addr: usize) -> u8 {
-        self.vram[addr - VRAM_START]
+        self.vram[addr]
     }
 
     pub fn read_vram16(&self, addr: usize) -> u16 {
-        LittleEndian::read_u16(&self.vram[addr - VRAM_START..])
+        LittleEndian::read_u16(&self.vram[addr..])
     }
 
-    pub fn write_vram(&mut self, value: u8, addr: usize) {
-        self.vram[addr - VRAM_START] = value;
+    pub fn write_vram(&mut self, addr: usize, value: u8) {
+        self.vram[addr] = value;
     }
 
-    pub fn write_vram16(&mut self, value: u16, addr: usize) {
-        LittleEndian::write_u16(&mut self.vram[addr - VRAM_START..], value);
+    pub fn write_vram16(&mut self, addr: usize, value: u16) {
+        LittleEndian::write_u16(&mut self.vram[addr..], value);
     }
 
     pub fn read_oam(&self, addr: usize) -> u8 {
-        self.oam[addr - OAM_START]
+        self.oam[addr]
     }
 
     pub fn read_oam16(&self, addr: usize) -> u16 {
-        LittleEndian::read_u16(&self.oam[addr - OAM_START..])
+        LittleEndian::read_u16(&self.oam[addr..])
     }
 
     pub fn write_oam(&mut self, addr: usize, value: u8) {
-        self.oam[addr - OAM_START] = value;
+        self.oam[addr] = value;
     }
 
     pub fn write_oam16(&mut self, addr: usize, value: u16) {
-        LittleEndian::write_u16(&mut self.oam[addr - OAM_START..], value);
+        LittleEndian::write_u16(&mut self.oam[addr..], value);
     }
 
-    fn set_lcd_ctrl(&mut self, value: u8) {
-        self.switchbg  = value & (1 << 0) != 0;
-        self.bgmap     = value & (1 << 3) != 0;
-        self.bgtile    = value & (1 << 4) != 0;
-        self.switchlcd = value & (1 << 4) != 0;
-    }
-
-    fn lcd_ctrl(&self) -> u8 {
+    pub fn read_lcd_ctrl(&self) -> u8 {
         let bit0 = if self.switchbg  { 0x01 } else { 0x00 };
         let bit3 = if self.bgmap     { 0x08 } else { 0x00 };
         let bit4 = if self.bgtile    { 0x10 } else { 0x00 };
@@ -194,12 +191,19 @@ impl Gpu {
         bit0 | bit3 | bit4 | bit7
     }
 
-    fn read_lcd_stat(&self) -> u8 {
-        (if self.coincidence_int { 1 << 6 } else { 0 }) |
-        (if self.mode2oam_int    { 1 << 5 } else { 0 }) |
-        (if self.mode1vblank_int { 1 << 4 } else { 0 }) |
-        (if self.mode0hblank_int { 1 << 3 } else { 0 }) |
-        (if self.line == 0       { 1 << 2 } else { 0 }) | // TODO LY @ FF44 == LYC @ FF45
+    pub fn write_lcd_ctrl(&mut self, value: u8) {
+        self.switchbg  = value & (1 << 0) != 0;
+        self.bgmap     = value & (1 << 3) != 0;
+        self.bgtile    = value & (1 << 4) != 0;
+        self.switchlcd = value & (1 << 4) != 0;
+    }
+
+    pub fn read_lcd_stat(&self) -> u8 {
+        (if self.coincidence_int  { 1 << 6 } else { 0 }) |
+        (if self.mode2oam_int     { 1 << 5 } else { 0 }) |
+        (if self.mode1vblank_int  { 1 << 4 } else { 0 }) |
+        (if self.mode0hblank_int  { 1 << 3 } else { 0 }) |
+        (if self.line == self.lyc { 1 << 2 } else { 0 }) |
         match self.mode {
             Mode::Oam    => 0b10,
             Mode::Vram   => 0b11,
@@ -208,7 +212,7 @@ impl Gpu {
         }
     }
 
-    fn write_lcd_stat(&mut self, value: u8) {
+    pub fn write_lcd_stat(&mut self, value: u8) {
         self.coincidence_int = value & (1 << 6) != 0;
         self.mode2oam_int    = value & (1 << 5) != 0;
         self.mode1vblank_int = value & (1 << 4) != 0;
@@ -240,7 +244,7 @@ impl Gpu {
             //color = self.pal[self.tileset[tile][y][x]];
 
             // plot the pixel to the buffer
-            self.fb[fb_offset] = 0;//color;
+            // self.fb[fb_offset] = 0;//color;
             fb_offset += 1;
 
             // when this tile ends, read another
@@ -255,18 +259,19 @@ impl Gpu {
     }
 }
 
-fn set_palette(pal: &mut Vec<Vec<u8>>, value: u8) {
-    for i in 0..4 {
-        pal[i] = match (value >> (i * 2)) & 0b11 {
-            0 => WHITE.to_vec(),
-            1 => LGRAY.to_vec(),
-            2 => DGRAY.to_vec(),
-            3 => BLACK.to_vec(),
-            _ => unreachable!()
-        }
-    }
-}
+// fn set_palette(pal: &mut Vec<Vec<u8>>, value: u8) {
+//     for i in 0..4 {
+//         pal[i] = match (value >> (i * 2)) & 0b11 {
+//             0 => WHITE.to_vec(),
+//             1 => LGRAY.to_vec(),
+//             2 => DGRAY.to_vec(),
+//             3 => BLACK.to_vec(),
+//             _ => unreachable!()
+//         }
+//     }
+// }
 
+#[derive(Debug)]
 enum Mode {
     Oam,    // 2
     Vram,   // 3
